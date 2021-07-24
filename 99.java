becb24f3e5d922cc10769420bbe0d619619eb3f5
Prathamesh0451 <prathuberi@gmail.com>
2021-07-24 16:21:15 +0530

    99 - Recover tree, modifications, correct.

--- 99.java
+++ 99.java
@@ -16,51 +16,79 @@
 class Solution {
     public void recoverTree(TreeNode root) {
         
-        recoverTree(root, null);
+        
+       recoverTreeHelper(root);
         
     }
     
-    public void recoverTree(TreeNode node, TreeNode parent){
+    public void recoverTreeHelper(TreeNode node){
+  
+        if(node == null) return;
         
-        if(node == null){
-            return;
-        }else{
-            
-            if(node.left != null){
-                 if(parent != null && node.left.val > parent.val){
-                    swap(node.left,parent);
-                    return;
-                }else  if(node.left.val > node.val){
-                    swap(node.left, node);
-                    return;
-                }
-                
-                else{
-                    recoverTree(node.left, node);
-                }
-                
-            }
-            
-           if(node.right != null){
-               
-               
-               if(parent != null && node.right.val > parent.val){
-                   swap(node.right, parent);
-                   return;
-               }else if(node.right.val > node.val){
-                   swap(node.right, node);
-                   return;
-               }
-                   else{
-                   recoverTree(node.right, node);
-               }
-               
-               
-           } 
+    TreeNode lmax = searchLeftTree(node.left);
+    TreeNode rmax = searchRightTree(node.right);
+        
+    if(lmax != null && rmax != null && lmax.val > node.val && rmax.val < node.val){
+        swap(lmax, rmax);
+        return;
+    }else if(lmax!= null && lmax.val > node.val){
+        swap(lmax, node);
+        return;
+    }else if(rmax != null && rmax.val < node.val){
+        swap(rmax, node);
+        return;
+    }else{
+        recoverTree(node.left);
+        recoverTree(node.right);
+    }
+        
+        
+    }
+    
+    TreeNode searchLeftTree(TreeNode lef){
+        if(lef == null ){
+            return null;
+        }
+        if(lef.left == null && lef.right == null){
+            return lef;
+        }
+        TreeNode toReturn = lef;
+        TreeNode lefty = searchLeftTree(lef.left);
+        TreeNode righty = searchLeftTree(lef.right);
+        if(lefty != null && lefty.val > lef.val){
+             toReturn = lefty;
         }
         
+        if(righty != null && righty.val > toReturn.val){
+            toReturn = righty;
+        }
+        
+        return toReturn;
+        
     }
     
+    TreeNode searchRightTree(TreeNode righ){
+        if(righ == null){
+            return null;
+        }
+        if(righ.right ==null && righ.left == null){
+            return righ;
+        }
+        
+        TreeNode toReturn = righ;
+        TreeNode lefty = searchRightTree(righ.left);
+        TreeNode righty = searchRightTree(righ.right);
+        if(lefty != null && lefty.val < righ.val){
+             toReturn = lefty;
+        }
+        
+        if(righty != null && righty.val < toReturn.val){
+            toReturn = righty;
+        }
+        
+        return toReturn;
+        
+    }
     public void swap(TreeNode orig, TreeNode exchange){
         int vall = orig.val;
         orig.val = exchange.val;
